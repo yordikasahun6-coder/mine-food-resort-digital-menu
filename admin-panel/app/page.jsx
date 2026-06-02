1add59164b6bd9494cde1e2ddb0deba45f5d5fa7
YORDANOS <yordikasahun6@gmail.com>
2026-06-02 16:09:33 +0300

    Add root redirect page

--- admin-panel/app/page.jsx
+++ admin-panel/app/page.jsx
@@ -1,13 +1,10 @@
 'use client'
 
 import { useEffect } from 'react'
-import { useRouter } from 'next/navigation'
 
 export default function HomePage() {
-  const router = useRouter()
-  
   useEffect(() => {
-    router.push('/admin/login')
+    window.location.href = '/admin/login'
   }, [])
   
   return (
